c3a849001baabf22d9f456bda395164cbd3daca1
mohamedcamel <mohamedgameil002@gmail.com>
2025-03-13 11:14:23 +0000

    AggiuntaLink

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import {useParams} from 'react-router';
+import {Link, useParams} from 'react-router';
 
 export const Detail = () => {
   const{id } = useParams();
@@ -39,6 +39,8 @@ export const App = () => {
           >
             Cambia
           </button>
+
+          <Link to="/frontend-rocks/dettaglio/1">Link alla pagina dettaglio</Link>
 
           <p className="text-center">
             Modifica <code>src/App.tsx</code> e salva per testare l'hot reload
